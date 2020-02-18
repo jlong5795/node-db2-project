4a74e2ed3a0ff89f375d4e27ff2be9236085923c
Jason Long <jlong5795@gmail.com>
2020-02-18 16:50:56 -0600

    Completed Stretch

--- migrations/20200218143250_cars-table.js
+++ migrations/20200218143250_cars-table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('cars', tbl => {
-        tbl.increments();
+        tbl.increments('id');
 
         tbl
             .string('VIN')
